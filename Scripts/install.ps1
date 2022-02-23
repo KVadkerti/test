662d3f0986a8eb3f8f053f9117ff719f4081dca9
kvadkerti <kvadkerti@172.26.52.9>
2022-02-23 10:27:47 +0100

    aktualizace

--- Scripts/install.ps1
+++ Scripts/install.ps1
@@ -1,10 +1,12 @@
 ﻿write-host "Install scripts :"
 write-host 'Path ' $FilePath
 
+write-host $(System.DefaultWorkingDirectory)
+
 #&"$PSScript\1.ps1"
 #&"$PSScript\2.ps1"
 #&"$PSScript\3.ps1"
 
-& $FilePath'\PSconnect.ps1'
-& $FilePath'\Import-PBIX-File.ps1'
-& $FilePath'\PSdisconnect.ps1'
+#& $FilePath'\PSconnect.ps1'
+#& $FilePath'\Import-PBIX-File.ps1'
+#& $FilePath'\PSdisconnect.ps1'
