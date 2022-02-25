3a80636ded12497aee06e9f415dc04a799675009
kvadkerti <kvadkerti@172.26.52.9>
2022-02-25 10:03:03 +0100

    aktualizace

--- Scripts/TE_build_bim_file.bat
+++ Scripts/TE_build_bim_file.bat
@@ -2,4 +2,6 @@ echo "Build .bim file"
 
 REM D:\a\1\s\TabularEditor\TabularEditor.exe "D:\a\1\a\DimModel" -B "D:\a\1\a\NewDimModel.bim"
 
-\TabularEditor\TabularEditor.exe "D:\a\1\a\DimModel" -B "D:\a\1\a\NewDimModel.bim"
+cd D:\a\1\s\TabularEditor\
+
+TabularEditor.exe "D:\a\1\a\DimModel" -B "D:\a\1\a\NewDimModel.bim"
