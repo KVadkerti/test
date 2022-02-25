2885c83bd8678de404ed4a251d1b27acb5eae0b5
kvadkerti <kvadkerti@172.26.52.9>
2022-02-25 11:22:46 +0100

    AKTUALIZACE

--- Scripts/TE_run_BPA.ps1
+++ Scripts/TE_run_BPA.ps1
@@ -1,7 +1,12 @@
 Write-Host "Run a Best Practice Analysis"
 
-$p = Start-Process -filePath $(Build.SourcesDirectory)\TabularEditor\TabularEditor.exe `
+Write-Host $Env:BUILD_SOURCESDIRECTORY
+Write-Host $Env:BUILD_ARTIFACTSTAGINGDIRECTORY
+#$(Build.SourcesDirectory
+#$(Build.ArtifactStagingDirectory)
+
+$p = Start-Process -filePath $Env:BUILD_SOURCESDIRECTORY\TabularEditor\TabularEditor.exe `
        -Wait -NoNewWindow -PassThru `
-       -argumentList "`"$(Build.ArtifactStagingDirectory)\DQ5.bim`" -A -G"
+       -argumentList "`"$Env:BUILD_ARTIFACTSTAGINGDIRECTORY\DQ5.bim`" -A -G"
 
 exit $p.ExitCode
