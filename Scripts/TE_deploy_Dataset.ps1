b751445f9685170bbf072f7553a5b0dc2fba7b65
kvadkerti <kvadkerti@172.26.52.9>
2022-02-25 12:33:16 +0100

    akt

--- Scripts/TE_deploy_Dataset.ps1
+++ Scripts/TE_deploy_Dataset.ps1
@@ -6,9 +6,10 @@ Write-Host "Build.ArtifactStagingDirectory: " $Env:BUILD_ARTIFACTSTAGINGDIRECTOR
 #$(Build.ArtifactStagingDirectory)
 
 # Cmd Line D:\a\1\s\TabularEditor\TabularEditor.exe "D:\a\1\a\NewDimModel.bim" -DEPLOY "$(PBIConnection)" "NewDim"
+#-DEPLOY `"Data Source=$(DataSource);User ID=$(UserID);Password=$(UserPassword)`"
 
 $p = Start-Process -filePath $Env:BUILD_SOURCESDIRECTORY\TabularEditor\TabularEditor.exe `
        -Wait -NoNewWindow -PassThru `
-       -argumentList "`"$Env:BUILD_ARTIFACTSTAGINGDIRECTORY\NewDimModel.bim`" -DEPLOY `"Data Source=$(DataSource);User ID=$(UserID);Password=$(UserPassword)`" `"NewDim`""
+       -argumentList "`"$Env:BUILD_ARTIFACTSTAGINGDIRECTORY\NewDimModel.bim`" -DEPLOY `"Data Source=$Env:DATASOURCE;User ID=$Env:USERID;Password=$Env:USERPASSWORD`" `"NewDim`""
 
 exit $p.ExitCode
